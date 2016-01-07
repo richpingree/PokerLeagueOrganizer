4e3013068a508829972a2ad2b1f5b689b4aff454
richpingree <richpingree@fullsail.edu>
2016-01-06 23:25:09 -0600

    added create user function and functions to check that textfields are valid
    added create user function and functions to check that textfields are
    valid (emails and passwords match)

--- parse-starter-project-1.10.0/ParseStarterProject-Swift/ParseStarterProject/RegisterViewController.swift
+++ parse-starter-project-1.10.0/ParseStarterProject-Swift/ParseStarterProject/RegisterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class RegisterViewController: UIViewController {
 
@@ -27,9 +28,55 @@ class RegisterViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //check that emails match
+    func checkEmailMatch() {
+        if emailTxt.text != confirmEmailTxt.text{
+            let alert = UIAlertView(title: "Email address mismatch", message: "Please check email address and try again", delegate: nil, cancelButtonTitle: "OK")
+            alert.show()
+        }
+    }
+    
+    //check that passwords match
+    func checkPasswordMatch(){
+        if passTxt.text != confirmPassTxt.text{
+            let alert = UIAlertView(title: "Password mismatch", message: "Please check and enter again", delegate: nil, cancelButtonTitle: "OK")
+            alert.show()
+        }
+    }
+    //check that all fields are complete
+    func checkFieldComplete() {
+        if userTxt.text!.isEmpty || emailTxt.text!.isEmpty || confirmEmailTxt.text!.isEmpty || passTxt.text!.isEmpty || confirmEmailTxt.text!.isEmpty {
+            let alert = UIAlertView(title: "All Fields are Required", message: "Please check the fields and try again", delegate: nil, cancelButtonTitle: "OK")
+            alert.show()
+        } else {
+            checkEmailMatch()
+            checkPasswordMatch()
+        }
+    }
+    
+    
+    //register user
+    func signUp() {
+        let user = PFUser()
+        user.username = userTxt!.text
+        user.email = emailTxt!.text
+        user.password = passTxt!.text
+        
+        user.signUpInBackgroundWithBlock{
+            (success: Bool, error: NSError?) -> Void in
+            if let error = error {
+                _ = error.userInfo["error"] as? NSString
+                let alert = UIAlertView(title: "Ooops", message: "Username is already taken", delegate: nil, cancelButtonTitle: "OK")
+                alert.show()
+            } else {
+                NSLog("Sign Up Success!")
+            }
+        }
+    }
+    
 
     @IBAction func loginBtn(sender: AnyObject) {
-        
+        checkFieldComplete()
     }
     /*
     // MARK: - Navigation
